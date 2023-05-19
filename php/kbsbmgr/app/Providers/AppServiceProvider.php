bb8f38e10496a2c41e672a2352bc39fcdbd3598b
Ruben <ruben@chessdevil.net>
2023-05-19 15:04:51 +0200

    AppServiceProvide added forceHttps

--- php/kbsbmgr/app/Providers/AppServiceProvider.php
+++ php/kbsbmgr/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Statamic\Statamic;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,7 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Statamic::script('app', 'cp');
-        // Statamic::style('app', 'cp');
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
